cae6f0392a06ab6af3e8f076faf574bbc80710b4
Octiii <sirocti@gmail.com>
2023-04-13 14:13:30 +0000

    Set up the reservation model and registered it in the admin file.

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    persons = models.IntegerField() 
+    date = models.DateField() 
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
+        
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import reservation
+
 
 # Create your views here.
+
+from reservation.models import reservation 
+
+
+def reserv_table(request):
+    pass
