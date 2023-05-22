83b276aa1b262e1e4ae0753f7457f03e06d4836c
Octiii <sirocti@gmail.com>
2023-05-22 14:18:29 +0000

    Added javascript to enforce a character limit on the number and persons input fields.

--- reservation/models.py
+++ reservation/models.py
@@ -7,8 +7,8 @@ from datetime import datetime
 class reservation(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(default='Your email here.')
-    phone = models.IntegerField(default='0', blank=True)
-    persons = models.IntegerField(default='0', blank=True) 
+    phone = models.IntegerField(max_length=12, default='0', blank=True)
+    persons = models.IntegerField(max_length=2, default='0', blank=True) 
     date = models.DateTimeField(default=datetime.now, blank=True) 
 
     def __str__(self):
