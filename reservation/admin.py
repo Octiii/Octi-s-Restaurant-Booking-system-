76ca11f4aca2b91d32f13ebf19abac8c645cf428
Octiii <sirocti@gmail.com>
2023-06-12 15:22:02 +0000

    Adding new model and rendering it trough class based view.

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import reservation
+from .models import reservation, dish
 
 # Register your models here.
 
 admin.site.register(reservation)
+admin.site.register(dish)
