d546def1f32aeb8d7f4ef9d057e1d5c6bdc599ab
Octiii <sirocti@gmail.com>
2023-06-26 09:49:40 +0000

    Deplymnet issues

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/Octi-s-Restaurant-Booking-system-/reservation/static/'
+STATIC_URL = '/static/'
 STATIC_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
