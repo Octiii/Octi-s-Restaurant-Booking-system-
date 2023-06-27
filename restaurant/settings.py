9fee1dc5019a68b0bbeb0a049aa35dc7fb3992f1
Octiii <sirocti@gmail.com>
2023-06-27 11:49:35 +0000

    Deployment debuging, might be an issue with pating.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/reservation')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reservation/static/reservation')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
