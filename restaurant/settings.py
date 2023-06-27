e4f564bdf19936ae2eb64caf756448fb5dfb4fd1
Octiii <sirocti@gmail.com>
2023-06-27 11:28:26 +0000

    Deployment debuging, might be a cloudinary problem, env import moved before cloudinary ones.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -14,12 +14,16 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
+if os.path.isfile('env.py'):
+    import env
+
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-if os.path.isfile('env.py'):
-    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,8 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reservation/static/reservation')]
+#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
