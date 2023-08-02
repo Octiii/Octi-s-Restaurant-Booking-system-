65008044031c193577152ebf0a7b4ece23767646
Octiii <sirocti@gmail.com>
2023-08-02 13:45:53 +0000

    adding authentification functunality.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("reservation.urls"), name="reservation.url"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
