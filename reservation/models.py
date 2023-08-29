3450462fb2881ff6980d4868862378846b523793
Octiii <sirocti@gmail.com>
2023-08-29 12:10:25 +0000

    Removed author from dish model as it did not work and would have to erase the data base, maybe will do it in the future.

--- reservation/models.py
+++ reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
